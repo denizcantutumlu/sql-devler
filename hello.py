fb8b509c91c9be60c66606b4fb1b3a0c43e57575
denizcantutumlu <dcantutumlu@icloud.com>
2023-02-21 21:30:28 +0300

    ilk commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  8 21:19:34 2023
+
+@author: zesir_000
+"""
+
+print("hello tatlıım")
---
+++ zeynepsu.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  8 22:24:17 2023
+
+@author: zesir_000
+"""
+
+print('zeynepsu')
